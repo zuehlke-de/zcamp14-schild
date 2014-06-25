a956bb151ad298076a62dc84ec74eaaf02d374c7
Kai Hergenroether <kahe@zuehlke.com>
2014-06-25 14:37:24 +0000

    - added Ble class, handling handset interaction via Bluetooth Low Energy.

---
+++ RaspberryQtApp/zcamp14-schild-qt/Ble.h
@@ -0,0 +1,28 @@
+#ifndef ZCAMP14_SCHILD_RASPI_BLE_H__
+#define ZCAMP14_SCHILD_RASPI_BLE_H__
+
+
+#include <QObject>
+
+
+//! Represents the Bluetooth Low Energy receiver/transmitter.
+class Ble : public QObject {
+    Q_OBJECT
+
+  public:
+    Ble();
+
+  signals:
+    void roomNameUpdate(QString roomName);
+    void occupantNamesInvalidated();
+    void occupantNameUpdate(QString occupantName);
+
+  private slots:
+    void processTimer();
+
+  private:
+    int counter_;
+};
+
+
+#endif   // ZCAMP14_SCHILD_RASPI_BLE_H__
