f48b6bfb6c7f316c7c3882859c87fec6fbd85729
vod <vod@zuehlke.com>
2014-06-25 12:17:32 +0200

    Initial commit RestServer "siegfried"

---
+++ RestServer/src/main/java/com/zuehlke/camp2014/iot/brokers/schild/logic/MoveLogic.java
@@ -0,0 +1,18 @@
+package com.zuehlke.camp2014.iot.brokers.schild.logic;
+
+import com.zuehlke.camp2014.iot.brokers.schild.domain.Move;
+
+public class MoveLogic {
+
+	public void processMoveMessage(Move move) {
+		System.out.println("Process message "+move);
+		
+		// TODO: Get domain object
+		
+		// TODO: Implement logic
+		//move.setMoveId(UUID.randomUUID().toString());
+			
+		
+	}
+
+}
