63291af4360a8c22aa9338985b9ab17b2a5fb5f6
vod <vod@zuehlke.com>
2014-06-26 15:38:47 +0200

    Migration to jersey 2.9

---
+++ RestServer/src/main/java/com/zuehlke/camp2014/schild/siegfried/Siegfried.java
@@ -0,0 +1,9 @@
+package com.zuehlke.camp2014.schild.siegfried;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class Siegfried extends ResourceConfig {
+	public Siegfried() {
+        packages("com.zuehlke.camp2014.schild.siegfried");
+    }
+}
