4fe1265e8218754394e604e3d2f7b419cdeb8f9e
soniro <Nina.Rothenberg@gmail.com>
2014-06-26 16:20:20 +0200

    Separate kill script.

--- RaspberryQtApp/scripts/startCouplingAndQtApp.sh
+++ RaspberryQtApp/scripts/startCouplingAndQtApp.sh
@@ -1,4 +1,4 @@
-kill -9 $(ps aux | grep 'coupling' | awk '{print $2}')
+~/zcamp14-schild/RaspberryQtApp/scripts/killProcesses.sh
 kill -9 $(ps aux | grep 'startx' | awk '{print $2}')
 rm /tmp/ble_coupling
 ~/zcamp14-schild/RaspberryQtApp/scripts/makeAndRunQtApp.sh &
