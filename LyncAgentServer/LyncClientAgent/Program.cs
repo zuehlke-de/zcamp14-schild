3d40dcbce7e02f9134b8d817f64f57372f92d721
wmax <wmax@zuehlke.com>
2014-06-26 16:34:54 +0200

    code cleanup

--- LyncAgentServer/LyncClientAgent/Program.cs
+++ LyncAgentServer/LyncClientAgent/Program.cs
@@ -17,36 +17,5 @@ namespace LyncClientAgent
         {
             new LyncLocationSetter().Run();
         }
-
-        static void TestAD()
-        {
-            using (var context = new PrincipalContext(ContextType.Domain, "ads.zuehlke.com"))
-            {
-                UserPrincipal up = new UserPrincipal(context);
-                using (var searcher = new PrincipalSearcher(up))
-                {
-                    up.SamAccountName = "wmax";
-                    foreach (var result in searcher.FindAll())
-                    {
-                        DirectoryEntry de = result.GetUnderlyingObject() as DirectoryEntry;
-                        if (de.Properties["samAccountName"].Value.Equals("wmax"))
-                        {
-                            foreach (var val in de.Properties.PropertyNames)
-                            {
-                                Console.WriteLine(val + ": " + de.Properties[val.ToString()].Value);
-                            }
-                        }
-                        /*
-                        Console.WriteLine("First Name: " + de.Properties["givenName"].Value);
-                        Console.WriteLine("Last Name : " + de.Properties["sn"].Value);
-                        Console.WriteLine("SAM account name   : " + de.Properties["samAccountName"].Value);
-                        Console.WriteLine("User principal name: " + de.Properties["userPrincipalName"].Value);
-                        Console.WriteLine();
-                         */
-                    }
-                }
-            }
-            Console.ReadLine();
-        }
     }
 }
